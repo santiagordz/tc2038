389acb46c1b98acc0307caabe72add7164d79bff
David Langarica <david.langarica@gmail.com>
2023-11-13 12:43:26 -0600

    push

--- ActInt2_Equipo10/main.py
+++ ActInt2_Equipo10/main.py
@@ -5,11 +5,10 @@ from distancia_mas_corta import encontrar_central_mas_cercana
 
 # Código existente para Problema 2 y 3
 file_structure = open_file("entrada1.txt")
-file_lines = file_structure  # Asumiendo que file_structure es una lista de líneas
+file_lines = file_structure[1]  # Asumiendo que file_structure es una lista de líneas
 
 # Problema 2 y 3: usar la primera línea para el problema del agente viajero y el árbol de expansión mínima
-file_line_1 = file_lines[0]
-adj_matrix_1 = build_adjacency_matrix_from_file_lines(file_line_1)
+adj_matrix_1 = build_adjacency_matrix_from_file_lines(file_lines)
 
 route, distance = slime_mold(adj_matrix_1)
 
@@ -21,14 +20,17 @@ print("Problema 3")
 msp = min_span_tree(adj_matrix_1)
 print(msp)
 
+file_lines1 = file_structure[3]
+print(file_lines1)
+
 # Problema 4: leer las centrales y la nueva ubicación
 # Asumiendo que las centrales están en las líneas subsiguientes y la última línea es la nueva ubicación
-centrales = [tuple(map(float, line.split(','))) for line in file_lines[1:-1]]
-nueva_central = tuple(map(float, file_lines[-1].split(',')))
+centrales = [tuple(map(float, line.split(","))) for line in file_lines[1:-1]]
+nueva_central = tuple(map(float, file_lines[-1].split(",")))
 
-central_cercana, distancia = encontrar_central_mas_cercana(
-    centrales, nueva_central)
+central_cercana, distancia = encontrar_central_mas_cercana(centrales, nueva_central)
 
 print("Problema 4")
 print(
-    f"La central más cercana está en {central_cercana} a una distancia de {distancia} km")
+    f"La central más cercana está en {central_cercana} a una distancia de {distancia} km"
+)
