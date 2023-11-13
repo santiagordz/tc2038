8cac9758ced35f7bbbdbfac4139d128fd50d4abd
JP-coder2000 <70972830+JP-coder2000@users.noreply.github.com>
2023-11-13 12:17:58 -0600

    Adding part 4

---
+++ ActInt2_Equipo10/distancia_mas_corta.py
@@ -0,0 +1,20 @@
+import math
+
+
+def calcular_distancia(punto1, punto2):
+    """Calcula la distancia euclidiana entre dos puntos."""
+    return math.sqrt((punto1[0] - punto2[0])**2 + (punto1[1] - punto2[1])**2)
+
+
+def encontrar_central_mas_cercana(centrales, nueva_central):
+    """Encuentra la central más cercana a la nueva ubicación."""
+    distancia_minima = float('inf')
+    central_mas_cercana = None
+
+    for central in centrales:
+        distancia = calcular_distancia(central, nueva_central)
+        if distancia < distancia_minima:
+            distancia_minima = distancia
+            central_mas_cercana = central
+
+    return central_mas_cercana, distancia_minima
