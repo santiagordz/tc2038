09ae230d46d28162179f95fbd6f0920c84eb45f0
santiagordz <santiromur@gmail.com>
2023-11-18 10:27:24 -0600

    last version

--- ActInt2_Equipo10/main.py
+++ ActInt2_Equipo10/main.py
@@ -1,9 +1,12 @@
 from file_operations import build_adjacency_matrix_from_file_lines, open_file
 from forma_optima import *
+from flujo_maximo import *
 from tsp import slime_mold, print_route
 from distancia_mas_corta import encontrar_central_mas_cercana
 
 NUM_FILES = 3
+centrales = None
+nueva_central = None
 
 if __name__ == "__main__":
     for i in range(NUM_FILES):
@@ -18,6 +21,11 @@ if __name__ == "__main__":
         print_route(route, distance)
 
         print("Problema 3")
+        max_flox_matrix = build_adjacency_matrix_from_file_lines(file_structure[2])
+        max_flow = ford_fulkerson(max_flox_matrix)
+        print(f"El flujo máximo es {max_flow} unidades.")
+
+        print("Problema 4")
         msp = min_span_tree(adj_matrix)
         print(msp)
         centrales = file_structure[3]
@@ -25,8 +33,6 @@ if __name__ == "__main__":
         central_cercana, distancia = encontrar_central_mas_cercana(
             centrales, nueva_central
         )
-
-        print("Problema 4")
         print(
             f"La central más cercana es {central_cercana} con una distancia de {distancia} unidades."
         )
